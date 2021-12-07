edc294d729d07f4616a84140596f887930427bc3
chuheeseung <youverygood@naver.com>
2021-12-07 12:56:39 +0900

    2021.12.07

--- shopping/main.js
+++ shopping/main.js
@@ -15,42 +15,43 @@ function onAdd() {
     input.focus(); //사용자가 다시 와서 input 다시 입력할 수 있음
 }
 
+let id = 0; //UUID
+
 function createItem(text) {
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
+    itemRow.setAttribute('data-id', id);
 
-    const item = document.createElement('div');
-    item.setAttribute('class', 'item');
+    itemRow.innerHTML = `
+        <div class="item">
+            <span class="item__name">${text}</span>
+            <button class="item__delete">
+                <i class="fas fa-trash-alt" data-id=${id}></i>
+            </button>
+        </div>
+        <div class="item__divider"></div>
+    `;
 
-    const span = document.createElement('span');
-    span.setAttribute('class', 'item__name');
-    span.innerText = text;
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('class', 'item__delete');
-    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-    deleteBtn.addEventListener('click', () => {
-        items.removeChild(itemRow);
-    })
-
-    const itemDivider = document.createElement('div');
-    itemDivider.setAttribute('class', 'item__divider');
-
-    item.appendChild(span);
-    item.appendChild(deleteBtn);
-
-    itemRow.appendChild(item);
-    itemRow.appendChild(itemDivider);
+    id++;
 
     return itemRow;
 }
 
 addBtn.addEventListener('click', () => {
     onAdd();
-})
+});
 
 input.addEventListener('keypress', (event) => {
     if(event.key === 'Enter') {
         onAdd();
     }
-})
+});
+
+items.addEventListener('click', event => {
+    const id = event.target.dataset.id;
+
+    if(id) {
+        const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+        toBeDeleted.remove();
+    }
+});
